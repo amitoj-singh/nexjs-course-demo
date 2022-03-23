b9c30cb7111784dbccf942f44d7823a8eb878dc7
Amitoj Singh <amitoj.98@gmail.com>
2022-03-23 14:26:57 +0530

    pushing code to git

---
+++ pages/index.js
@@ -0,0 +1,48 @@
+import Head from "next/head";
+import { MongoClient } from "mongodb";
+
+import MeetupList from "../components/meetups/MeetupList";
+
+const HomePage = (props) => {
+  return (
+    <>
+      <Head>
+        <title>React Meetups</title>
+        <meta
+          name="description"
+          content="Browse a huge list of highly active React meetups!"
+        />
+      </Head>
+      <MeetupList meetups={props.meetups} />
+    </>
+  );
+};
+
+// export async function getServerSideProps() {}
+
+export async function getStaticProps() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://dbadmin:dbadmin@cluster0.xznkd.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
+  return {
+    props: {
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
+    },
+    revalidate: 1,
+  };
+}
+
+export default HomePage;
